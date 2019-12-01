9f94db887642a6d849f1ff5258a31670acdad3c9
Ian Baldwin <ian@iantbaldw.in>
2019-12-01 10:50:41 -0500

    Added enum to protos

--- protos/message_pb2.py
+++ protos/message_pb2.py
@@ -4,6 +4,7 @@
 
 import sys
 _b=sys.version_info[0]<3 and (lambda x:x) or (lambda x:x.encode('latin1'))
+from google.protobuf.internal import enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
@@ -20,9 +21,44 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\rmessage.proto\"P\n\rSimpleMessage\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07\x64\x65\x63imal\x18\x02 \x01(\x02\x12\r\n\x05\x63ount\x18\x03 \x01(\x05\x12\x11\n\tis_simple\x18\x04 \x01(\x08\"H\n\x10\x44\x65pendentMessage\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x0esimple_message\x18\x02 \x01(\x0b\x32\x0e.SimpleMessageb\x06proto3')
+  serialized_pb=_b('\n\rmessage.proto\"g\n\rSimpleMessage\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07\x64\x65\x63imal\x18\x02 \x01(\x02\x12\r\n\x05\x63ount\x18\x03 \x01(\x05\x12\x11\n\tis_simple\x18\x04 \x01(\x08\x12\x15\n\x05\x63olor\x18\x05 \x01(\x0e\x32\x06.Color\"H\n\x10\x44\x65pendentMessage\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x0esimple_message\x18\x02 \x01(\x0b\x32\x0e.SimpleMessage*J\n\x05\x43olor\x12\x11\n\rCOLOR_UNKNOWN\x10\x00\x12\r\n\tCOLOR_RED\x10\x01\x12\x0f\n\x0b\x43OLOR_GREEN\x10\x02\x12\x0e\n\nCOLOR_BLUE\x10\x03\x62\x06proto3')
 )
 
+_COLOR = _descriptor.EnumDescriptor(
+  name='Color',
+  full_name='Color',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='COLOR_UNKNOWN', index=0, number=0,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='COLOR_RED', index=1, number=1,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='COLOR_GREEN', index=2, number=2,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='COLOR_BLUE', index=3, number=3,
+      serialized_options=None,
+      type=None),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=196,
+  serialized_end=270,
+)
+_sym_db.RegisterEnumDescriptor(_COLOR)
+
+Color = enum_type_wrapper.EnumTypeWrapper(_COLOR)
+COLOR_UNKNOWN = 0
+COLOR_RED = 1
+COLOR_GREEN = 2
+COLOR_BLUE = 3
 
 
 
@@ -61,6 +97,13 @@ _SIMPLEMESSAGE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='color', full_name='SimpleMessage.color', index=4,
+      number=5, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -74,7 +117,7 @@ _SIMPLEMESSAGE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=17,
-  serialized_end=97,
+  serialized_end=120,
 )
 
 
@@ -111,13 +154,15 @@ _DEPENDENTMESSAGE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=99,
-  serialized_end=171,
+  serialized_start=122,
+  serialized_end=194,
 )
 
+_SIMPLEMESSAGE.fields_by_name['color'].enum_type = _COLOR
 _DEPENDENTMESSAGE.fields_by_name['simple_message'].message_type = _SIMPLEMESSAGE
 DESCRIPTOR.message_types_by_name['SimpleMessage'] = _SIMPLEMESSAGE
 DESCRIPTOR.message_types_by_name['DependentMessage'] = _DEPENDENTMESSAGE
+DESCRIPTOR.enum_types_by_name['Color'] = _COLOR
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 SimpleMessage = _reflection.GeneratedProtocolMessageType('SimpleMessage', (_message.Message,), {
